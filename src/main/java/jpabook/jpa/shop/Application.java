77c99fbe69ba1a4d409f2c6898d6e163747f6f0d
ym1085 <youngmin1085@gmail.com>
2022-03-29 22:22:04 +0900

    initialize commit

---
+++ src/main/java/jpabook/jpa/shop/Application.java
@@ -0,0 +1,13 @@
+package jpabook.jpa.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/jpabook/jpa/shop/ApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.jpa.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
