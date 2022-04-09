faa9b9265582427e989bed5f6b7cf746294d8e78
ym1085 <youngmin1085@gmail.com>
2022-04-09 14:36:08 +0900

    FEAT: 엔티티 설계 주의사항
    1. @OneToOne, @ManyToOne fetch type LAZY로 변경
    2. 연관관계 편의 메서드 작성
    3. 엔티티 설계 기본 적인 주의사항 정리

--- src/main/java/jpabook/jpa/shop/domain/Order.java
+++ src/main/java/jpabook/jpa/shop/domain/Order.java
@@ -13,14 +13,15 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+//    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<OrderItem>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -28,4 +29,20 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문 상태 [ORDER, CANCEL]
+
+    //==연관관계 편의 메서드==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        this.orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
