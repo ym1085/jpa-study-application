874cefeb03f7074a4a62627f2cf2ac258f55e751
ym1085 <youngmin1085@gmail.com>
2022-04-16 18:15:55 +0900

    FEAT: 주문 서비스 개발
    1. DDD 개발 방식 사용

--- src/main/java/jpabook/jpa/shop/domain/OrderItem.java
+++ src/main/java/jpabook/jpa/shop/domain/OrderItem.java
@@ -1,13 +1,16 @@
 package jpabook.jpa.shop.domain;
 
 import jpabook.jpa.shop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
